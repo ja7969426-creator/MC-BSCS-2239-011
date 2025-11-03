691285a0d783a7851da5589e7d9ac0eda0bd63bb
MC-BSCS-239-011 <ja7969426@gmail.com>
2025-11-03 18:58:16 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+print("hello world")
+print(3)
+
+print(3+2)
+
+print("name",3)
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruit = ["apple", "banana", "cherry"]
+print(fruit)
+
+thisdict = {
+    "UNI":  "TIMES UNIVERSITY MULTAN",
+     "Deparatment": "computer science",
+  "Name": "Jawad Ahmad",
+  "reg no": "MC-BSCS-239-011",
+  "semseter":  5
+ 
+  
+  
+}
+print(thisdict)
+
